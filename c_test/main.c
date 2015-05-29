29152baffa8fe5bb863ba9196b86744d8c75da27
mac <mac@macdeMacBook-Air.local>
2015-05-29 09:50:41 +0800

    Initial Commit

---
+++ c_test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c_test
+//
+//  Created by mac on 15/5/29.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
